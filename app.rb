7c0b71678d08701795451ee147b196c8b3a32818
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-04-28 11:58:49 +0100

    changed  into game.player attribute

--- app.rb
+++ app.rb
@@ -9,23 +9,26 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_2_hp = $player_2.hp
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    @player_1_hp = $game.player_1.hp
+    @player_2_hp = $game.player_2.hp
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    Game.new.attack($player_2)
-    @player_2_hp = $player_2.hp
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    $game.attack($game.player_2)
+    @player_1_hp = $game.player_1.hp
+    @player_2_hp = $game.player_2.hp
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,12 +5,8 @@ describe Game do
   let(:player_2) { double :player_2 }
 
   describe "#attack" do
-    it "attacks opponent" do
-      subject = Game.new
-      expect(subject).to respond_to(:attack)
-    end
-
     it 'reduces HP of a player' do
+      subject = (Game.new(player_1, player_2))
       expect(player_1).to receive(:loses_HP)
       subject.attack(player_1)
     end
