14e78eb13b1e44b80df4ee3fb19aafed311178c7
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-28 15:05:49 +0100

    Adds #switch_turns method to Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,29 @@ require_relative 'player'
 
 class Game
 
-  attr_reader :player_1, :player_2
-  
+  attr_reader :players
+  attr_reader :active_player
+
   def initialize(player1, player2)
-    @player_1 = player1
-    @player_2 = player2
+    @players = [player1, player2]
+    @active_player = player_1
   end
 
   def attack(player)
     player.loses_HP
+  end
+
+  def switch_turns
+    @players[0], @players[1] = @players[1], @players[0]
+    @active_player = @players[0]
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,13 @@ describe Game do
   let(:player_1) { double :player_1 }
   let(:player_2) { double :player_2 }
 
+  describe "#switch_turns" do
+    it 'responds to switch_turns' do
+      subject = (Game.new(player_1, player_2))
+      expect(subject).to respond_to :switch_turns
+    end
+  end
+
   describe "#attack" do
     it 'reduces HP of a player' do
       subject = (Game.new(player_1, player_2))
